0ade256a587dadbe88d97f6b94564636229db338
marcos314 <marcoswesley314@gmail.com>
2021-03-06 19:34:26 -0300

    Feat: API funcionando

--- config/routes.js
+++ config/routes.js
@@ -1,54 +1,53 @@
 const express = require('express')
 const routes = express.Router()
 
-let login =[
-   { Usuario:'tech', Senha:'123'}
+// let login =[
+//    { Usuario:'tech', Senha:'123'}
 
-]
-let token =[
-    {token:'token_de_acesso'}
-]
-// pegar informação através de auth e apresentar como resposta
+// ]
+// let token =[
+//     {token:'token_de_acesso'}
+// ]
+
+// Pegar informação através de get passando um token auth e apresentar como lista
 routes.get('/pesos', (req,res)=>{
+
     const headers = req.headers
-    
-    const fs = require("fs")
-    
+    const fs = require("fs")    
       
     const jsonData = JSON.parse(fs.readFileSync("./pesos.json" , "utf8")) 
     if(headers.token === 'token_de_acesso'){
-        return res.json(jsonData)
-    
+        return res.json(jsonData)    
     }
+
     else{
         return res.status(400).end()
     }
     
 })
 // filtrar por lote
-routes.get('/pesos/lote', (req,res)=>{
+routes.get('/pesos/:lote', (req,res)=>{
+
     const headers = req.headers
-    const body = req.body.idlote
-    const fs = require("fs")
-    function buscarlote (value,body) {
-        if(value.idlote===body)
-            return value;
+    const id_lote = parseInt(req.params.lote)
+    const fs = require("fs")    
+    
+    function buscarLote(value) {        
+        return value.idlote === id_lote;        
     }
       
     const jsonData = JSON.parse(fs.readFileSync("./pesos.json" , "utf8")) 
     if(headers.token === 'token_de_acesso'){
-        
-        
-        const lote = jsonData.filter(buscarlote)
-        
-        return res.json(lote)
 
+        const lote = jsonData.filter(buscarLote)   
+        return res.json(lote)
     }
     else{
         return res.status(400).end()
-    }
-    
+    }    
 })
+
+
 //autenticação login
 routes.post('/login', (req,res,next) =>{
     const body = req.body
